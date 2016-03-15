1feda56ac711c87d98358edff0b25c026a43ebe5
Erik Madsen <beatmadsen@gmail.com>
2016-03-15 01:12:13 +0100

    EM: SecondController, WIP

--- src/main/java/com/madsen/rx/second/controller/SecondController.java
+++ src/main/java/com/madsen/rx/second/controller/SecondController.java
@@ -4,6 +4,9 @@ import com.madsen.rx.second.data.SecondDto;
 import com.madsen.rx.second.domain.Second;
 import com.madsen.rx.second.service.SecondService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,16 +47,37 @@ public class SecondController {
                 vo -> Optional.of(new SecondDto(vo.name, vo.address));
 
         executorService.submit(() -> {
-                    final Set<SecondDto> dtos = service.readAll()
-                            .stream()
-                            .map(second -> second.extract(extractor))
-                            .filter(Optional::isPresent)
-                            .map(Optional::get)
-                            .collect(Collectors.toSet());
+            final Set<SecondDto> dtos = service.readAll()
+                    .stream()
+                    .map(second -> second.extract(extractor))
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .collect(Collectors.toSet());
 
-                    result.setResult(dtos);
-                }
-        );
+            result.setResult(dtos);
+        });
+
+        return result;
+    }
+
+
+    @RequestMapping(value = "/second/{id}", method = RequestMethod.GET)
+    public DeferredResult<ResponseEntity<SecondDto>> getSingle(@PathVariable("id") final long id) {
+
+        final DeferredResult<ResponseEntity<SecondDto>> result = new DeferredResult<>();
+
+        final Function<Second.SecondVo, Optional<SecondDto>> extractor =
+                vo -> Optional.of(new SecondDto(vo.name, vo.address));
+
+        executorService.submit(() -> {
+
+            final ResponseEntity<SecondDto> entity = service.read(id)
+                    .flatMap(value -> value.extract(extractor))
+                    .map(dto1 -> new ResponseEntity<>(dto1, HttpStatus.OK))
+                    .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
+            result.setResult(entity);
+        });
 
         return result;
     }
